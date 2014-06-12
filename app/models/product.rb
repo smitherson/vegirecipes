a6db9007c416ef1484a40fb049da1e092c4e85c0
smitherson <lyubomirdinchev@gmail.com>
2014-06-12 11:21:23 +0300

    deleted:    960-Grid-System-master.zip 	deleted:    app/controllers/.recipes_controller.rb.swp 	modified:   app/models/product.rb 	modified:   app/views/recipes/_product_list.erb 	modified:   tastks.txt

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ActiveRecord::Base
+#    t.string   "name",                                        null: false
+  validates :name, presence: true
+#    t.decimal  "protein",            precision: 10, scale: 0, null: false
+  validates :protein, presence: true
+#    t.decimal  "fat",                precision: 10, scale: 0, null: false
+  validates :fat, presence: true
+#    t.decimal  "fiber",              precision: 10, scale: 0, null: false
+  validates :fiber, presence: true
+#    t.decimal  "calories",           precision: 10, scale: 0, null: false
+  validates :calories, presence: true
+#    t.integer  "carbs"
+  validates :carbs, presence: true
+
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_many :products_for_recipe
